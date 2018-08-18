b3656f9c2f05e9260877185ffaec6da387a37807
Zhang Wenxiao <forever.wx.zhang@gmail.com>
2018-08-18 06:47:28 +0000

    test useable

--- test/server.cpp
+++ test/server.cpp
@@ -6,7 +6,6 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <pthread.h>
 #include <chrono>
 #include <thread>
 #include <iomanip>
@@ -18,74 +17,76 @@
 
 using namespace std;
 
-struct sockaddr_in localAddr;
-struct sockaddr_in remoteAddr[5];
-socklen_t addrlen = sizeof(localAddr);
-
-void *ThreadReceiverFunction(void *socket) {
-    cout<<"Receiver Thread Created!"<<endl;
+void ThreadReceiverFunction(int sock, int i) {
+    cout<<"Receiver Thread0 Created!"<<endl;
     char buffer[RES_SIZE];
-    int sock = *((int*)socket);
+    int resCount = 0;
 
     while (1) {
-        for(int i = 0; i < 5; i++) {
             memset(buffer, 0, sizeof(buffer));
-            recvfrom(sock, buffer, PACKET_SIZE, 0, (struct sockaddr *)&remoteAddr[0], &addrlen);
+            recvfrom(sock, buffer, PACKET_SIZE, 0, NULL, NULL);
             int markerNum = *(int*)&(buffer[8]);
-            cout<<"result received with "<<markerNum<<" markers"<<endl;
-        }
+            resCount += markerNum;
+            cout<<"result "<<i<<" received with "<<markerNum<<" markers, in total "<<resCount<<endl;
     }
 }
 
-void *ThreadSenderFunction(void *socket) {
+void ThreadSenderFunction(int *sock, struct sockaddr_in *remoteAddr) {
     cout << "Sender Thread Created!" << endl;
     char buffer[PACKET_SIZE];
-    int sock = *((int*)socket);
 
     ifstream in("request", ios::in | ios::binary);
     in.read(buffer, 48713);
     in.close();
 
     for(int test = 0; test < 60 * 8; test++) {
-        this_thread::sleep_for(chrono::milliseconds(1000));
+        this_thread::sleep_for(chrono::milliseconds(60000));
 
         for(int i = 0; i < 5; i++) {
-            sendto(sock, buffer, 48713, 0, (struct sockaddr *)&remoteAddr[i], addrlen);
+            sendto(sock[i], buffer, 48713, 0, (struct sockaddr *)&remoteAddr[i], sizeof(remoteAddr[i]));
             cout<<"request sent to container "<<i<<endl;
         }
     }    
 }
 
 int main(int argc, char *argv[]) {
-    pthread_t senderThread, receiverThread;
-    int sockUDP;
+    struct sockaddr_in localAddr[5];
+    struct sockaddr_in remoteAddr[5];
+    int sockUDP[5];
 
-    memset((char*)&localAddr, 0, sizeof(localAddr));
-    localAddr.sin_family = AF_INET;
-    localAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    localAddr.sin_port = htons(51710);
+    for(int i = 0; i < 5; i++) {
+        memset((char*)&localAddr[i], 0, sizeof(localAddr[i]));
+        localAddr[i].sin_family = AF_INET;
+        localAddr[i].sin_addr.s_addr = htonl(INADDR_ANY);
+        int port = 51710+i;
+        localAddr[i].sin_port = htons(port);
+
+        if((sockUDP[i] = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            cout<<"ERROR opening udp socket"<<endl;
+            exit(1);
+        }
+        if(bind(sockUDP[i], (struct sockaddr *)&localAddr[i], sizeof(localAddr[i])) < 0) {
+            cout<<"ERROR on udp binding"<<endl;
+            exit(1);
+        }
+    }
 
     struct hostent *hp;
-    hp = gethostbyname("192.168.50.223");
+    hp = gethostbyname("127.0.0.1");
     for(int i = 0; i < 5; i++) {
         memset((char*)&remoteAddr[i], 0, sizeof(remoteAddr[i]));
         remoteAddr[i].sin_family = AF_INET;
-        remoteAddr[i].sin_port = htons(51717+i);
+        int port = 51717+i;
+        remoteAddr[i].sin_port = htons(port);
         memcpy((void*)&remoteAddr[i].sin_addr, hp->h_addr_list[0], hp->h_length);
     }
 
-    if((sockUDP = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        cout<<"ERROR opening udp socket"<<endl;
-        exit(1);
-    }
-    if(bind(sockUDP, (struct sockaddr *)&localAddr, sizeof(localAddr)) < 0) {
-        cout<<"ERROR on udp binding"<<endl;
-        exit(1);
-    }
+    thread receiverThread[5];
+    for(int i = 0; i < 5; i++) 
+        receiverThread[i] = thread(ThreadReceiverFunction, sockUDP[i], i);
+    thread senderThread(ThreadSenderFunction, sockUDP, remoteAddr);
 
-    pthread_create(&receiverThread, NULL, ThreadReceiverFunction, (void *)&sockUDP);
-    pthread_create(&senderThread, NULL, ThreadSenderFunction, (void *)&sockUDP);
-
-    pthread_join(receiverThread, NULL);
-    pthread_join(senderThread, NULL);
+    for(int i = 0; i < 5; i++) 
+        receiverThread[i].join();
+    senderThread.join();
 }
